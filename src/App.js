06c7a8cb6668f8c8f8c30b25dab74519d941d26a
Evan K <EvanK311@gmail.com>
2020-11-06 19:39:09 -0800

    reformat files, started search and sort form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 
 import './App.css';
-import tablerow from "./components/tablerow";
 
-class App extends Component {
 
-}
-render() {
+function App() {
   return (
     <div>
-     <Table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Image</th>
-                        <th scope="col">Image</th>
-                        <th scope="col">Phone</th>
-                        <th scope="col">Email</th>
-                    </tr>
-        </thead>
-        <tbody>
-          {tablerow /}
-        </tbody>
-        </table>
+      <h2>yo</h2>
     </div>
   );
 }
