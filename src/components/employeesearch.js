7337b794b9688ca1849a467bd9988c38008aa526
Evan K <EvanK311@gmail.com>
2020-11-06 20:12:04 -0800

    added search form

--- src/components/employeesearch.js
+++ src/components/employeesearch.js
@@ -24,8 +24,8 @@ export default class EmployeeSearch extends React.Component {
     };
 
     //Sorts alphabetically by first name
-    alphaSort = () => {
-        this.setState({results: this.state.results.sort((a, b) => a.name.first.localeCompare(b.name.first))})
+    abcSort = () => {
+        this.setState({employees: this.state.results.sort((a, b) => a.name.first.localeCompare(b.name.first))})
     }
     //this kind of works, but it filters the NEXT set of results instead of the CURRENT set of results
     filterRes = () => {
@@ -48,7 +48,7 @@ export default class EmployeeSearch extends React.Component {
     //function that calls the API search here
     searchEmps = query => {
         API.search(query).then(res => {
-            this.setState({results: res.data.results,
+            this.setState({employees: res.data.results,
                             displayEmps: res.data.results})
         }).catch(err => console.log(err));
     }
@@ -63,7 +63,7 @@ export default class EmployeeSearch extends React.Component {
                 handleFormSubmit={this.handleFormSubmit}
                 handleInputChange={this.handleInputChange}
                 />
-                <button className="btn btn-primary" onClick={this.alphaSort}>Sort Alphabetically by Name</button>
+                <button className="btn btn-primary" onClick={this.abcSort}>Sort Alphabetically</button>
                 <table className="table">
                 <ResultList results={this.state.displayUsers} />
                 </table>
