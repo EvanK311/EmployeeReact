45137f7fe7e8e08dc88e464742adf1e79eac43af
Evan K <EvanK311@gmail.com>
2020-11-09 21:04:00 -0800

    search form progress

--- src/App.js
+++ src/App.js
@@ -24,6 +24,44 @@ class App extends React.Component {
           // .then(res => this.setState({ result: res.data }))
           // .catch(err => console.log(err));
   
+          handleFormSubmit = event => {
+            event.preventDefault();
+            this.filterUpdated(this.state.data);
+          }
+  
+          handleInputChange = (e) => {
+            let { value } = e.target
+            value = value.toLowerCase()
+            if (value) {
+              const filtered = this.state.data.filter(user => (user.name.first.toLowerCase().includes(value) || (user.name.last.toLowerCase().includes(value))))
+              this.setState({ data: filtered, searchTerm: value })
+            } else {
+              this.setState({ data: [...this.state.backupData], searchTerm: value })
+            }
+          }
+  
+          sortNames = () => {
+            // get the current employees ( probably this.state.data )
+            const sortedNames = this.state.data.sort((a, b) => {
+              if (a.name.first > b.name.first) {
+                return 1;
+              }
+              if (a.name.first < b.name.first) {
+                return -1;
+              }
+              return 0;
+            })
+            this.setState(prevState => ({
+              ...prevState,
+              data: sortedNames
+            }))
+          }
+          
+            buttonPress = () => {
+            this.sortNames()
+            console.log(this.state.data)
+          }
+  
           render() {
             return (
               <div>
