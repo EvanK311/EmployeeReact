a9172b89a0be9e7679b4b9775b450b61cb76541c
Evan K <EvanK311@gmail.com>
2020-11-09 20:07:02 -0800

    API call firing and list displaying

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 
 import './App.css';
-import EmployeeSearch from "./components/employeesearch";
+import React, { Component } from 'react'
+import EmployeeResult from "./components/employeeresult"
+import API from "./utils/API";
 
-function App() {
-  return <EmployeeSearch />;
-}
-
-export default App;
+class App extends React.Component {
+    state = {
+     data: [],
+     search: ""
+    }
+  
+  componentDidMount = () => {
+        API.search().then(res => {
+          // console.log(res);
+          this.setState({ data: res.data.results })
+          console.log({data:res.data});
+        }).catch(err => {
+            console.log(err);
+          })
+      
+      }
+          // .then(res => this.setState({ result: res.data }))
+          // .catch(err => console.log(err));
+  
+          render() {
+            return (
+              <div>
+                <EmployeeResult data={this.state.data} />
+              </div >
+            )
+          }
+        }
+    export default App;
--- src/components/employeeresult.js
+++ src/components/employeeresult.js
@@ -1,26 +1,40 @@
 import React from "react";
 
 function EmployeeResult(props) {
-    console.log("props.employees", props.employees)
-    return (
-    <table className="table">
-        <br />
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Phone Number</th>
-          <th>Pic</th>
-        </tr>
-          {props.employees.map(result => (
+
+
+
+  return <div>
+
+      <table className="table">
+          <thead>
               <tr>
-              <td>{result.name.first} {result.name.last}</td>
-              <td>{result.email}</td>
-              <td>{result.phone}</td>
-              <td><img src={result.picture.thumbnail}/></td>
+                  <th scope="col">Employee #</th>
+                  <th scope="col">Image</th>
+                  <th scope="col">Name </th>
+                  <th scope="col">Phone</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">DOB</th>
               </tr>
-          ))}
-    </table>  
-    );
-  }
+          </thead>
+          <tbody>
+
+              {props.data.map((data, index) => {
+                  return (
+                      <tr key={index}>
+                          <th scope="row">{index + 1}</th>
+                          <td><img src={data.picture.thumbnail} alt="employee pictures" /></td>
+                          <td>{data.name.first} {data.name.last}</td>
+                          <td>{data.phone}</td>
+                          <td>{data.email}</td>
+                          <td>{data.dob.date}</td>
+                      </tr>
+                  )
+
+              })}
+          </tbody>
+      </table>
+  </div>
+}
 
 export default EmployeeResult;
